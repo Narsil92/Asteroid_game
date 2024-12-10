b886548083c6e165851d0135293f7532b69f5dd5
Narsil92 <pblus23@gmail.com>
2024-12-10 14:52:32 +0100

    added possiblity to move back with down arrow to player

--- player.py
+++ player.py
@@ -32,7 +32,10 @@ class Player(CircleShape):
             self.rotate(dt)   
        
         if keys[pygame.K_UP]:    
-           self.move(dt)   
+           self.move(dt)
+
+        if keys[pygame.K_DOWN]:
+            self.move(dt)      
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
